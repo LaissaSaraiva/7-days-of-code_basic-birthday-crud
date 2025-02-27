3350a80095c81a2b57ae0d98d650235c75cdfef4
Laissa Cunha <laissa_saraiva_k2@whirlpool.com>
2025-02-27 20:27:49 -0300

    feat: day 3: add localStorage validation

--- js/script.js
+++ js/script.js
@@ -1,11 +1,42 @@
 const form = document.querySelector(".js-form");
-console.log("🚀 ~ form:", form);
 
 const nameInput = document.querySelector("#fname");
 const birthdayInput = document.querySelector("#birthday");
+const table = document.querySelector("#table");
+
+let users = new Array();
+
+window.onload = function (e) {
+  users = JSON.parse(localStorage.getItem("users")) || [];
+  console.log("🚀 ~ users:", users)
+  users.forEach(user => {
+    listNewUser(user)
+  });
+};
+
+function listNewUser(user) {
+  const userName = document.createElement("p")
+  userName.innerHTML = user.name
+  const userBirthday = document.createElement("p")
+  userBirthday.innerHTML = user.birthday
+  table.appendChild(userName)
+  table.appendChild(userBirthday)
+}
+
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
-  console.log("~ Name:", nameInput.value);
-  console.log("~ Birthday:", birthdayInput.value);
+
+  const person = {
+    name: nameInput.value,
+    birthday: birthdayInput.value
+  }
+
+  if (localStorage.hasOwnProperty("users")) {
+    users = JSON.parse(localStorage.getItem("users"));
+  }
+
+  users.push(person)
+  localStorage.setItem("users", JSON.stringify(users))
+  listNewUser(person)
 });
